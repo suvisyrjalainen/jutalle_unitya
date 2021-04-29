abc3a25df9107aa763ecb72cb8481bbc9232e19a
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2021-04-29 16:36:45 +0300

    opetustunti

---
+++ Assets/omat_jutut/Scripts/player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private float horisontaalinenPyorinta = 0;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController hahmokontrolleri = GetComponent<CharacterController>();
+
+        //eteen ja sivulle liikkuminen
+        float horizontal = Input.GetAxis("Horizontal") * 5;
+        float vertical = Input.GetAxis("Vertical") * 5;
+        Vector3 nopeus = new Vector3(horizontal, 0, vertical);
+
+        //kääntyminen
+        horisontaalinenPyorinta += Input.GetAxis("Mouse X") * 3;
+        transform.localRotation = Quaternion.Euler(0, horisontaalinenPyorinta, 0);
+        nopeus = transform.rotation * nopeus;
+
+        hahmokontrolleri.Move(nopeus * Time.deltaTime);
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+}
